6f8927a40695ce3b67374a059a6fcae16b797234
ivan.guerrero <guerrero.ivan.789@gmail.com>
2023-07-05 01:37:51 -0500

    Ajuste log de crons

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cron = require("node-cron");
 const express = require("express");
-const {parse, addHours, addMinutes} = require("date-fns");
+const { parse, addHours, addMinutes } = require("date-fns");
 const axios = require('axios');
 const clientPromise = require("./mongodb");
 const HOST_URL = process.env.HOST_URL || "http://localhost:3000";
@@ -15,7 +15,6 @@ const execFindSchedules = async () => {
   if (trxs?.length > 0) {
     trxs.forEach(schedules => {
       schedules?.offices?.forEach(office => {
-        console.log("Cedula: " + schedules.dni + " Oficina: " + office.name + " Dates: ");
         const config = {
           method: 'post',
           url: `${HOST_URL}/api/agendarCitasGlobal`,
@@ -32,6 +31,7 @@ const execFindSchedules = async () => {
         }
         axios(config)
           .then(function (response) {
+            console.log("execFindSchedules Cedula: " + schedules.dni + " Oficina: " + office.name + " Dates: " + schedules.dates[0] + "-" + schedules.dates[schedules.dates.length - 1]);
             console.log(response.data);
           }).catch(function (error) {
             console.error(error);
@@ -50,7 +50,6 @@ const execNewQuotas = async () => {
   if (trxs) {
     trxs.forEach(schedules => {
       schedules?.offices?.forEach(office => {
-        console.log("New Quota Cedula: " + schedules.dni + " Oficina: " + office.name);
         const config = {
           method: 'post',
           url: `${HOST_URL}/api/newQuota`,
@@ -66,6 +65,7 @@ const execNewQuotas = async () => {
         }
         axios(config)
           .then(function (response) {
+            console.log("New Quota Cedula: " + schedules.dni + " Oficina: " + office.name);
             console.log(response.data);
           }).catch(function (error) {
             console.error(error);
@@ -92,26 +92,26 @@ cron.schedule("*/45 * * * * *", async function () {
       newQuotaEnd = addHours(newQuotaEnd, newQuotaDuration.value);
       break;
     case 'MINUTES':
-        newQuotaEnd = addMinutes(newQuotaEnd, newQuotaDuration.value);
-        break;
+      newQuotaEnd = addMinutes(newQuotaEnd, newQuotaDuration.value);
+      break;
     default:
       break;
   }
-  if(current >= newQuotaStart && current <= newQuotaEnd){
-    cronsObj.newQuota = cron.schedule(newQuotaCronStr, execNewQuotas, {scheduled:true})
-  }else{
-    if(cronsObj?.newQuota) cronsObj.newQuota?.stop();
+  if (current >= newQuotaStart && current <= newQuotaEnd) {
+    cronsObj.newQuota = cron.schedule(newQuotaCronStr, execNewQuotas, { scheduled: true })
+  } else {
+    if (cronsObj?.newQuota) cronsObj.newQuota?.stop();
   }
   const trxs = await db
     .collection("trxs")
-    .find({ 
+    .find({
       status: 'PENDING',
       type: 'FIND_SCHEDULES'
     }).toArray();
   if (trxs) {
-    cronsObj.findSchedules = cron.schedule(findSchedulesCronStr,  execFindSchedules, {scheduled:true})
-  }else{
-    if(cronsObj?.findSchedules) cronsObj.findSchedules?.stop();
+    cronsObj.findSchedules = cron.schedule(findSchedulesCronStr, execFindSchedules, { scheduled: true })
+  } else {
+    if (cronsObj?.findSchedules) cronsObj.findSchedules?.stop();
   }
 });
 
